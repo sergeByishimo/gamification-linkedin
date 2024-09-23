474e227cbf464d9394fb45018041bf3bd0d5a043
Serge Byishimo <sergebyishimo@gmail.com>
2024-09-23 15:09:05 +0200

    feat: Linkedin Connector - Meeds-io/MIPs#141
    Work in Progress

--- gamification-linkedin-webapp/src/main/webapp/vue-app/connectorEventExtensions/services.js
+++ gamification-linkedin-webapp/src/main/webapp/vue-app/connectorEventExtensions/services.js
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import * as linkedinConnectorService from '../linkedinAdminConnectorExtension/js/LinkedinConnectorService.js';
+import * as linkedinConnectorService from '../linkedinAdminConnectorExtension/js/LinkedInConnectorService.js';
 
 if (!Vue.prototype.$linkedinConnectorService) {
   window.Object.defineProperty(Vue.prototype, '$linkedinConnectorService', {
